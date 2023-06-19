12544d6c18cc7b423b6e78a59acb75e0693fe86a
Brian Ashton <ashtobd@gmail.com>
2023-06-19 07:05:40 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(array, names) {
+    return array.filter(item => item.toUpperCase() === names.toUpperCase());
+}
+
+function fuzzyMatch(array, names) {
+    return array.filter(letters => letters.slice(0, 2) === names.slice(0, 2));
+}
+
+function matchName(array, string) {
+    return array.filter(item => item.name === string);
+}
